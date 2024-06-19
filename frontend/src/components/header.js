ea1cee543e9e62655e5bc3d9d8c3d616edcab7f9
a.saudabekov <a.saudabekov@7g.team>
2024-06-19 19:12:34 +0500

    2024 changes

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -24,7 +24,7 @@ const Header = () => {
                     <a href="https://www.instagram.com/samuryq_group/" target="_blank" rel="noopener noreferrer">
                         <img src={instagramLogo} alt="" />
                     </a>
-                    <a href="https://api.whatsapp.com/send?phone=87008725003" target="_blank" rel="noopener noreferrer">
+                    <a href="https://api.whatsapp.com/send?phone=77008725003" target="_blank" rel="noopener noreferrer">
                         <img src={whatsappLogo} alt="" />
                     </a>
                     {/*<a href="https://kaspi.kz/shop/p/napol-nyi-vozdushno-kompressionnyi-shiatsu-109781209/?c=511010000&ref=shared_link" target="_blank" rel="noopener noreferrer">*/}
